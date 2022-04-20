88f153b6ffb3e296c4bc494e283905baf1072b1d
Piotr Bujak <piotr.bujak@motorolasolutions.com>
2022-04-20 10:19:02 +0200

    fixed merge

--- Gitties.cs
+++ Gitties.cs
@@ -1,12 +1,11 @@
-<<<<<<< HEAD
-introduce(Console.WriteLine("Hello, I'm Gittie, im angry!! RooaaoaoaR!");
-=======
-introduce(Console.WriteLine("Hello, I'm Gittie! And i am very small");
+
+introduce(Console.WriteLine("Hello, I'm Gittie,And i am very small and im angry!! RooaaoaoaR!");
+
+
 
 
 add(int a, int b){
     return a + b;
 }
 
->>>>>>> ab12de104d417745693d058c11ff3c5f7bc8bec9
 joke()
